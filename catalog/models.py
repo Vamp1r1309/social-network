ae3a5a2c36bcfe796134f1962aca4df2b9f19641
Vamp1r1309 <shlikov.vadim@gmail.com>
2024-02-05 00:44:40 +0300

    Добавлены в модель Catalog: картинка статус публикации

--- catalog/models.py
+++ catalog/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class CatalogModel(models.Model):
+    """Модель продукты"""
+    class Status(models.IntegerChoices):
+        DRAFT = 0 # Черновик
+        PUBLISHED = 1 # Опубликовано
     name = models.CharField('Наименование',
                             max_length=100,
                             blank=True,
@@ -9,10 +13,30 @@ class CatalogModel(models.Model):
     description = models.TextField('Описание',)
     price = models.FloatField('Цена',
                               max_length=10)
-    quantity = models.IntegerField('Количество')
-    tags = models.ManyToManyField('TagPost', blank=True, related_name='tags')
+    quantity = models.PositiveIntegerField('Количество', 
+                                           default=1)
+    tags = models.ManyToManyField('TagPost', 
+                                  blank=True, 
+                                  related_name='tags')
+    image = models.ImageField('Фотография', 
+                              upload_to='static/media/', 
+                              blank=True, 
+                              null=True)
+    is_published = models.BooleanField('Публикация',
+                                       choices=Status.choices,
+                                       default=Status.DRAFT)
+    
+    class Meta:
+        verbose_name = 'Товар'
+        verbose_name_plural = 'Товары'
+        ordering = ('id',)
+
+    def __str__(self) -> str:
+        return self.name
+
 
 class TagPost(models.Model):
+    """Модель для тэгов продукта"""
     tag = models.CharField(max_length=100, db_index=True)
     slug = models.SlugField(max_length=150, unique=True, db_index=True)
 
